0c45fae9b3a53ed9bbefe7e93814b9b7bf026b06
Junho Kim <junhokim@usc.edu>
2020-03-03 13:06:24 -0800

    Added dial system

---
+++ TV Dial/Assets/Scripts/DialScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialScript : MonoBehaviour
+{
+    public Camera m_camera;
+    public GameObject m_dialTick;
+    private int m_nCurrChannel = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = m_camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                m_dialTick.transform.localEulerAngles += new Vector3(0.0f, 120.0f, 0.0f);
+                if(m_nCurrChannel == 3)
+                {
+                    m_nCurrChannel = 1;
+                } else
+                {
+                    m_nCurrChannel++;
+                }
+            }
+        }
+    }
+
+    public int GetChannel()
+    {
+        return m_nCurrChannel;
+    }
+}
---
+++ TV Dial/Assets/Scripts/TVManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TVManager : MonoBehaviour
+{
+    public GameObject m_goDial;
+    private DialScript m_dsDialScript;
+    private int m_nDesiredChannel = 1;
+    private float m_fDesiredAngle = 0.0f;
+    private float m_fPoints = 0.0f;
+    private float m_fMinAngleForPoints = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_dsDialScript = m_goDial.GetComponent<DialScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckWinCondition();
+    }
+
+    void CheckWinCondition()
+    {
+        if(m_nDesiredChannel == m_dsDialScript.GetChannel())
+        {
+            //when the channel is correct
+            float fCurrentAngle = 0.0f;
+            float fAngleDifference = Mathf.Abs(m_fDesiredAngle - fCurrentAngle);
+            if(fAngleDifference < m_fMinAngleForPoints)
+            {
+                m_fPoints += (m_fMinAngleForPoints = fAngleDifference);
+            }
+        }
+    }
+
+    void RandomizeWinCondition()
+    {
+        //randomize the angles
+        m_nDesiredChannel = Random.Range(1, 4);
+        m_fDesiredAngle = Random.Range(-70.0f, 70.0f);
+    }
+}
